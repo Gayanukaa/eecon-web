2b078f7e82cf2abed7ce3fcf77e033d90080b41a
janithsamee <janith.samee0@gmail.com>
2024-05-05 12:50:04 +0530

    #fixed time table page

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -16,56 +16,99 @@ export default function Program() {
                     </h1>
                 </div>
             </div>
-            <div className="py-16 bg-gray-100 flex flex-col items-center">
-                <ComingSoon></ComingSoon>
-                <div className="mt-8 w-3/4 bg-white shadow-md rounded-lg overflow-hidden">
+            <div></div>
+            <div className="py-16 bg-gray-100 flex flex-col items-center p-4">
+                <h2 className="text-xl sm:text-2xl font-bold">
+                    Program Time Table
+                </h2>
+                <div className="mt-8 sm:w-3/4 w-full bg-white shadow-md rounded-lg overflow-hidden">
                     <table className="w-full">
                         <thead className="bg-blue-900">
                             <tr>
-                                <th className="text-left py-3 px-4 text-white">TIME</th>
-                                <th className="text-left py-3 px-4 text-white">DESCRIPTION</th>
+                                <th className="text-left py-3 px-16 md:px-24 text-white">
+                                    TIME
+                                </th>
+                                <th className="text-left py-3 px-4 text-white">
+                                    DESCRIPTION
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr className="bg-gray-200">
                                 <td className="py-2 px-4">8.00 am – 8.30 am</td>
-                                <td className="py-2 px-4">Registration</td>
+                                <td className="py-2 px-4">
+                                    <b>Registration</b>
+                                </td>
                             </tr>
                             <tr className="bg-white">
                                 <td className="py-2 px-4">8.30 am – 8.45 am</td>
-                                <td className="py-2 px-4">Inauguration</td>
+                                <td className="py-2 px-4">
+                                    <b>Inauguration</b>
+                                </td>
                             </tr>
                             <tr className="bg-gray-200">
-                                <td className="py-2 px-4">8.45 am – 10.15 am</td>
-                                <td className="py-2 px-4">Keynote Speech 1 by Prof. Jason Mars, University of Michigan, USA</td>
+                                <td className="py-2 px-4">
+                                    8.45 am – 10.15 am
+                                </td>
+                                <td className="py-2 px-4">
+                                    <b>Keynote Speech 1</b> <br />
+                                    by Prof. Jason Mars, University of Michigan,
+                                    USA
+                                </td>
                             </tr>
                             <tr className="bg-white">
-                                <td className="py-2 px-4">10.15 am – 10.45 am</td>
-                                <td className="py-2 px-4">Morning Tea</td>
+                                <td className="py-2 px-4">
+                                    10.15 am – 10.45 am
+                                </td>
+                                <td className="py-2 px-4">
+                                    <b>Morning Tea</b>
+                                </td>
                             </tr>
                             <tr className="bg-gray-200">
-                                <td className="py-2 px-4">10.45 am – 12.30 pm</td>
-                                <td className="py-2 px-4">Technical Paper presentations 1 (Three Parallel Sessions - 5 papers per session and 20 minutes per presentation)</td>
+                                <td className="py-2 px-4">
+                                    10.45 am – 12.30 pm
+                                </td>
+                                <td className="py-2 px-4">
+                                    <b> Technical Paper presentations 1</b>{" "}
+                                    <br /> (Three Parallel Sessions - 5 papers
+                                    per session and 20 minutes per presentation)
+                                </td>
                             </tr>
                             <tr className="bg-white">
-                                <td className="py-2 px-4">12.30 pm – 1.30 pm</td>
-                                <td className="py-2 px-4">Lunch</td>
+                                <td className="py-2 px-4">
+                                    12.30 pm – 1.30 pm
+                                </td>
+                                <td className="py-2 px-4">
+                                    <b>Lunch</b>
+                                </td>
                             </tr>
                             <tr className="bg-gray-200">
                                 <td className="py-2 px-4">1.30 pm – 3.00 pm</td>
-                                <td className="py-2 px-4">Keynote Speech 2 Coming Soon.</td>
+                                <td className="py-2 px-4">
+                                    <b>Keynote Speech 2</b>
+                                    <br />
+                                    Coming Soon...
+                                </td>
                             </tr>
                             <tr className="bg-white">
                                 <td className="py-2 px-4">3.00 pm – 3.30 pm</td>
-                                <td className="py-2 px-4">Evening Tea</td>
+                                <td className="py-2 px-4">
+                                    <b>Evening Tea</b>
+                                </td>
                             </tr>
                             <tr className="bg-gray-200">
                                 <td className="py-2 px-4">3.30 pm – 5.15 pm</td>
-                                <td className="py-2 px-4">Technical Paper presentations 2 (Two Parallel Sessions - 5 papers per session and 20 minutes per presentation)</td>
+                                <td className="py-2 px-4">
+                                    <b>Technical Paper presentations 2</b>
+                                    <br /> (Two Parallel Sessions - 5 papers per
+                                    session and 20 minutes per presentation)
+                                </td>
                             </tr>
                             <tr className="bg-white">
                                 <td className="py-2 px-4">5.15 pm – 5.45 pm</td>
-                                <td className="py-2 px-4">Closing and Awards Ceremony</td>
+                                <td className="py-2 px-4">
+                                    <b>Closing and Awards Ceremony</b>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
