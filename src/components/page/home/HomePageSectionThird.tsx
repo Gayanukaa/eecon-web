f837f28a3cdea0f20f8aa40cbdd3bca34d7f7802
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2024-07-10 09:17:02 +0530

    Added IEEE global logo

--- src/components/page/home/HomePageSectionThird.tsx
+++ src/components/page/home/HomePageSectionThird.tsx
@@ -3,6 +3,7 @@ import EESOCLOGO from "../../../assets/logos/eesoc_logo.png";
 import UOMLOGO from "../../../assets/logos/uom_logo.jpg";
 import IESLOGO from "../../../assets/logos/ies_logo.png";
 import PESLOGO from "../../../assets/logos/pes_logo.png";
+import IEEEGLOBALLOGO from "../../../assets/logos/ieee_global_logo.png";
 import IEEELOGO from "../../../assets/logos/ieee_logo.webp";
 import RASLOGO from "../../../assets/logos/ras_logo.png";
 import Image from "next/image";
@@ -60,8 +61,23 @@ export default function HomePageSectionThird() {
           <h5 className="text-xl sm:text-2xl text-transform:uppercase cap font-medium">
             Technical Co-Sponsors
           </h5>
+          <p className="text-sm sm:text-base ml-8 sm:ml-16 ">IEEE</p>
+        </div>
+        <div className="flex  ml-16  basis-1/2 justify-end items-center float-right ">
+          <Image
+            // style={{ width: 75, height: 75 }}
+            src={IEEEGLOBALLOGO}
+            alt="IEEE"
+            className="w-16"
+          ></Image>
+        </div>
+      </div>
+      <div className="flex  flex-row w-full sm:w-3/4 w-max-1/2 pl-8 mt-4 text-left justify-content: space-between">
+        <div className="basis-1/2">
           <p className="text-sm sm:text-base ml-8 sm:ml-16 ">
             IEEE Sri Lanka Section
+            <br />
+            Sri Lanka
           </p>
         </div>
         <div className="flex  ml-16  basis-1/2 justify-end items-center float-right ">
